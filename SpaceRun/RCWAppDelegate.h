79c8ad1123be6f2ab537ecd96a90a6ad9e44c5f7
Michael Lorenzo <mlorenzo@metaltoad.com>
2014-07-21 20:43:00 -0700

    Initial Commit

---
+++ SpaceRun/RCWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCWAppDelegate.h
+//  SpaceRun
+//
+//  Created by Michael Lorenzo on 7/21/14.
+//  Copyright (c) 2014 MTM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpaceRun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpaceRun
+//
+//  Created by Michael Lorenzo on 7/21/14.
+//  Copyright (c) 2014 MTM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCWAppDelegate class]));
+    }
+}
